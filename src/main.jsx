7c6e134dcb00ce8e7929a8fceb0e0c2e70880436
Aycan <aycan.acn2020@gmail.com>
2024-08-10 14:18:37 +0200

    init repo with files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from "./components/Hello";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>
+);
