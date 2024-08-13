a10d85da2789fcbc99498acbc26aaee30bd78ce1
Aycan <aycan.acn2020@gmail.com>
2024-08-13 20:43:21 +0200

    add work component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PersonalInfo from "./components/PersonalInfo";
 import Resume from "./components/Resume";
 import Education from "./components/Education";
+import Work from "./components/Work";
 import "./styles/app.css";
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
 
   const handleEducationForm = (e) => {
     setEducation({ ...education, [e.target.name]: e.target.value });
+  };
+
+  const handleWorkForm = (e) => {
+    setWork({ ...work, [e.target.name]: e.target.value });
   };
 
   const [person, setPerson] = useState({
@@ -30,6 +35,16 @@ function App() {
     location: "Massachusetts",
   });
 
+  const [work, setWork] = useState({
+    company: "Google",
+    position: "Software Engineer",
+    startDate: "12-03-2024",
+    endDate: "present",
+    location: "Zurich",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vel ea, explicabo, expedita illo doloribus necessitatibus quaerat sapiente aliquid aperiam, hic corrupti aliquam doloremque numquam minus eius? Aperiam voluptates consectetur, fuga ipsa autem ut dolores voluptate illo dignissimos reiciendis natus nostrum saepe aspernatur optio, rerum minima sit temporibus esse qui eveniet. Nesciunt, aspernatur perferendis. Nesciunt animi illum deleniti similique voluptates modi vitae doloribus autem rem pariatur. Deserunt sed possimus harum nulla ratione eius! Nostrum, non unde deserunt, temporibus architecto magni exercitationem culpa possimus accusamus aperiam commodi voluptatum repellendus molestiae perferendis quis dolores? Doloremque possimus in soluta nam, animi cumque numquam!",
+  });
+
   return (
     <>
       <PersonalInfo person={person} handleForm={handlePersonForm} />
@@ -38,6 +53,7 @@ function App() {
         education={education}
         handleEducationForm={handleEducationForm}
       />
+      <Work work={work} handleWorkForm={handleWorkForm} />
     </>
   );
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,60 @@
+function Work({ work, handleWorkForm }) {
+  return (
+    <div className="work-experience-container">
+      <div className="work-experience">
+        <form action="">
+          <label htmlFor="company">Company</label>
+          <input
+            type="text"
+            name="company"
+            id="company"
+            value={work.company}
+            onChange={handleWorkForm}
+          />
+          <label htmlFor="position">Position</label>
+          <input
+            type="text"
+            name="position"
+            id="position"
+            value={work.position}
+            onChange={handleWorkForm}
+          />
+          <label htmlFor="start-date">Start Date</label>
+          <input
+            type="text"
+            name="start-date"
+            id="start-date"
+            value={work.startDate}
+            onChange={handleWorkForm}
+          />
+          <label htmlFor="end-date">End Date</label>
+          <input
+            type="text"
+            name="end-date"
+            id="end-date"
+            value={work.endDate}
+            onChange={handleWorkForm}
+          />
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            value={work.location}
+            onChange={handleWorkForm}
+          />
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            value={work.description}
+            onChange={handleWorkForm}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
