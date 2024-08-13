0c3d6759a2841a8b1da760752405e0a3c4fc6929
Aycan <aycan.acn2020@gmail.com>
2024-08-13 13:45:10 +0200

    add styling to personalinfo card

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -10,62 +10,64 @@ function PersonalInfo({
   handleCareer,
 }) {
   return (
-    <div className="personal-info">
-      <h2>Personal Info</h2>
-      <form>
-        <label htmlFor="firstname">Firstname </label>
-        <input
-          type="text"
-          name="firstname"
-          id="firstname"
-          value={person.firstname}
-          onChange={handleFirstName}
-        />
-        <label htmlFor="lastname">Lastname </label>
-        <input
-          type="text"
-          name="lastname"
-          id="lastname"
-          value={person.lastname}
-          onChange={handleLastName}
-        ></input>
+    <div className="personal-info-container">
+      <div className="personal-info">
+        <h2>Personal Info</h2>
+        <form>
+          <label htmlFor="firstname">Firstname </label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            value={person.firstname}
+            onChange={handleFirstName}
+          />
+          <label htmlFor="lastname">Lastname </label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            value={person.lastname}
+            onChange={handleLastName}
+          ></input>
 
-        <label htmlFor="career">Career </label>
-        <input
-          type="text"
-          name="career"
-          id="career"
-          value={person.career}
-          onChange={handleCareer}
-        ></input>
+          <label htmlFor="career">Career </label>
+          <input
+            type="text"
+            name="career"
+            id="career"
+            value={person.career}
+            onChange={handleCareer}
+          ></input>
 
-        <label htmlFor="email">Email </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={person.email}
-          onChange={handleEmail}
-        ></input>
+          <label htmlFor="email">Email </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={person.email}
+            onChange={handleEmail}
+          ></input>
 
-        <label htmlFor="phone-number">Phone Number </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={person.phone}
-          onChange={handlePhone}
-        ></input>
+          <label htmlFor="phone-number">Phone Number </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={person.phone}
+            onChange={handlePhone}
+          ></input>
 
-        <label htmlFor="address">Address </label>
-        <input
-          type="text"
-          name="address"
-          id="address"
-          value={person.address}
-          onChange={handleAddress}
-        ></input>
-      </form>
+          <label htmlFor="address">Address </label>
+          <input
+            type="text"
+            name="address"
+            id="address"
+            value={person.address}
+            onChange={handleAddress}
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
