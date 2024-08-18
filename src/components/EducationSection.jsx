bd727593f9f0b7ace48e1c024fa3900804723a04
Aycan <aycan.acn2020@gmail.com>
2024-08-18 17:54:01 +0200

    finish making educationsection editable

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,5 +1,8 @@
+import "../styles/education.css";
+
 function EducationSection({ studies, updateStudy }) {
   const handleInput = (e, studyId, field) => {
+    // In case of a date, don't append the '-' symbol to the updated state obj
     const updatedValue = e.target.textContent;
     updateStudy(studyId, field, updatedValue);
   };
@@ -11,29 +14,50 @@ function EducationSection({ studies, updateStudy }) {
         return (
           <div key={study.id} className="education-block" data-index={index}>
             <div className="education-info">
-              <div>
-                <div className="school-year-container">
-                  <p
-                    contentEditable
-                    onBlur={(e) => handleInput(e, study.id, "school")}
-                    suppressContentEditableWarning
-                    className="study-school"
-                  >
-                    {study.school}
-                  </p>
-                  <p>
-                    {study.startYear} - {study.endYear} | {study.location}
-                  </p>
-                </div>
-                <div className="degree-container">
-                  <p
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => handleInput(e, study.id, "degree")}
-                  >
-                    {study.degree}
-                  </p>
-                </div>
+              <div className="school-degree">
+                <p
+                  contentEditable
+                  onBlur={(e) => handleInput(e, study.id, "school")}
+                  suppressContentEditableWarning
+                  className="study-school"
+                >
+                  {study.school}
+                </p>
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleInput(e, study.id, "degree")}
+                >
+                  {study.degree}
+                </p>
+              </div>
+
+              <div className="study-year-location">
+                <p
+                  className="study-start-year"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleInput(e, study.id, "startYear")}
+                >
+                  {study.startYear}
+                </p>
+                <span className="seperator">-</span>
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleInput(e, study.id, "endYear")}
+                >
+                  {study.endYear}
+                </p>
+                <span className="seperator">|</span>
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  className="study-location"
+                  onBlur={(e) => handleInput(e, study.id, "location")}
+                >
+                  {study.location}
+                </p>
               </div>
             </div>
           </div>
