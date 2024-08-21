d7d18d14db7d790a5cf4b17f9c4d84224d05a5cb
Aycan <aycan.acn2020@gmail.com>
2024-08-21 18:42:03 +0200

    make page responsive

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -10,68 +10,66 @@ function ProjectSection({ projects, updateProjects, deleteProject }) {
   const [hiddenBtn, setHiddenBtn] = useState(false);
 
   return (
-    <>
-      <div
-        className="project-section"
-        onMouseEnter={() => setHiddenBtn(true)}
-        onMouseLeave={() => setHiddenBtn(false)}
-      >
-        <h2 className="section-header">Projects</h2>
-        {projects.map((project) => {
-          return (
-            <div key={project.id} className="project-block">
-              <div className="project-info-container">
-                <div className="project-name-link-container">
-                  <p
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => handleInput(e, project.id, "name")}
-                  >
-                    {project.name}
-                  </p>
-                  <p className="seperator"> | </p>
-                  <p
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => handleInput(e, project.id, "link")}
-                  >
-                    {project.link}
-                  </p>
-                </div>
-                <div className="project-stack-info-container">
-                  <p
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={(e) => handleInput(e, project.id, "stack")}
-                  >
-                    {project.stack}
-                  </p>
-                  <div className="delete-container">
-                    {hiddenBtn && (
-                      <button
-                        className="delete-btn"
-                        onClick={() => deleteProject(project.id)}
-                      >
-                        Delete
-                      </button>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="project-description-container">
+    <div
+      className="project-section"
+      onMouseEnter={() => setHiddenBtn(true)}
+      onMouseLeave={() => setHiddenBtn(false)}
+    >
+      <h2 className="section-header">Projects</h2>
+      {projects.map((project) => {
+        return (
+          <div key={project.id} className="project-block">
+            <div className="project-info-container">
+              <div className="project-name-link-container">
                 <p
                   contentEditable
                   suppressContentEditableWarning
-                  onBlur={(e) => handleInput(e, project.id, "description")}
+                  onBlur={(e) => handleInput(e, project.id, "name")}
                 >
-                  {project.description}
+                  {project.name}
+                </p>
+                <p className="seperator"> | </p>
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleInput(e, project.id, "link")}
+                >
+                  {project.link}
                 </p>
               </div>
+              <div className="project-stack-info-container">
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleInput(e, project.id, "stack")}
+                >
+                  {project.stack}
+                </p>
+                <div className="delete-container">
+                  {hiddenBtn && (
+                    <button
+                      className="delete-btn"
+                      onClick={() => deleteProject(project.id)}
+                    >
+                      Delete
+                    </button>
+                  )}
+                </div>
+              </div>
             </div>
-          );
-        })}
-      </div>
-    </>
+            <div className="project-description-container">
+              <p
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={(e) => handleInput(e, project.id, "description")}
+              >
+                {project.description}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
